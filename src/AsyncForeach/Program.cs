b12fe71c15e146162b3824aed8c22dac1d3758ad
ytyaru <ytyaru@outlook.jp>
2019-11-01 18:51:17 +0900

    初回コミット

---
+++ src/AsyncForeach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncForeach
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await AsyncForeach(GenerateAsync());
+        }
+        static async Task AsyncForeach(IAsyncEnumerable<int> items)
+        {
+            await foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static async IAsyncEnumerable<int> GenerateAsync()
+        {
+            for (int i = 0; i<10; i++)
+            {
+                yield return i;
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
